abf0b9879645e4b65baf1909c8ac4d6de3212630
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2025-06-10 21:19:12 +0200

    add app.module.ts and comment it

---
+++ I_updateEnv/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'; // import de notre premier decorateur
+import { AppController } from 'app.controller';
+import { AppService } from 'app.service';
+
+// Decorateur Module indique a NestJS ce qu'est AppModule
+@Module({
+    imports : [],
+    controllers : [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
