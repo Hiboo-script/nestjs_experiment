b4a0e6d12bf38f9cbbd0481f375a829d7bd82fb6
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2025-06-10 19:58:54 +0200

    add files of project and main.ts finish

---
+++ I_updateEnv/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core'; 
+import { AppModule } from './app.module'; // import du fichier app.module.ts
+
+async function bootstrap() {
+    // await permet d'attendre qu'un processus (une promesse) soit realise
+    const app = await NestFactory.create(AppModule); // instancie l'app Nest en suivant les regles de AppModule
+    await app.listen(3000);
+} 
+boostrap();
