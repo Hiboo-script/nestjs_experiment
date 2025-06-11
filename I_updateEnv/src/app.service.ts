a1b41ea78ab33f4dc3c34aa1b0cec06133e16df1
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2025-06-11 17:20:16 +0200

    chore : remove I_updateEnv (now HelloWorld_NestJS)

--- I_updateEnv/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable() // decorateur qui permet a NestJS de trouver AppService lors de l'injection dans AppController
-export class AppService {
-    // methode de AppService tres simple !
-    getHello(): string {
-        return "Hello World !";
-    }
-}
--- I_updateEnv/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core'; 
-import { AppModule } from './app.module'; // import du fichier app.module.ts
-
-async function bootstrap() {
-    // await permet d'attendre qu'un processus (une promesse) soit realise
-    const app = await NestFactory.create(AppModule); // instancie l'app Nest en suivant les regles de AppModule
-    await app.listen(3000);
-} 
-bootstrap();
