b4db7741f2f3d249e7becfa227d40b47d79b5b96
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2025-06-11 10:04:37 +0200

    finished app.controller.ts

---
+++ I_updateEnv/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from 'app.service';
+
+@Controller()
+export class AppController {
+    @Get() // Parametre une route GET
+    getHello(): string { // ts => indique le type retourne par la methode (securite)
+        return this.appService.getHello(); // appelle getHello() dans app.service.ts et retourne le resultat
+    }
+
+// Le constructeur sert ici a injecter en private la dependance AppService a la racine de this 
+constructor(private readonly appService: AppService) {}
+}
